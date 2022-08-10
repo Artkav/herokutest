688e38b9439046a68c33018f6cecbdcfda550825
ArtemKAV <kompaniiets.artyom@gmail.com>
2022-08-11 01:20:17 +0300

    Change settings

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -165,8 +165,8 @@ REST_FRAMEWORK = {
 
 # SMTP Configuration
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
